d2688d9035248db43fad9b793a70c306c7158fca
Vishal Chitrode <chitrodevishal@gmail.com>
2026-06-22 15:32:04 +0530

    Add comprehensive Math examples and explanations for enhanced learning resources

---
+++ 3.JS/3.4.Math/index.js
@@ -0,0 +1,55 @@
+console.log("Math in JS");
+console.log(Math.E);
+console.log(Math.LN10);
+console.log(Math.LN2);
+console.log(Math.LOG10E);
+console.log(Math.LOG10E);
+console.log(Math.PI);
+console.log(Math.LOG2E);
+console.log(Math.SQRT1_2);
+console.log(Math.SQRT2);
+console.log(Math.abs(-39));
+
+console.log(Math.round(3.4));
+console.log(Math.round(3.5));
+console.log(Math.round(3.6));
+
+console.log(Math.floor(4.3));
+console.log(Math.floor(4.8));
+
+console.log(Math.ceil(5.3));
+console.log(Math.ceil(5.8));
+
+console.log(Math.trunc(3.5));
+
+
+console.log(Math.random())
+
+// 0-9
+console.log(Math.floor(Math.random()*10))
+
+// 1-9
+console.log(Math.floor(Math.random()*9) +1)
+
+// 0-10
+console.log(Math.floor(Math.random()*10) +1)
+
+//1-6
+console.log(Math.floor((Math.random()*6))+1)
+
+console.log(Math.max(23, 34, 45,23, 45,56))
+console.log(Math.min(23, 34, 45,23, 45,56))
+console.log(Math.pow(2,4))
+console.log(2**4)
+console.log(Math.sqrt(45))
+
+let price = 23.23232345
+console.log(price.toFixed(4))
+console.log(typeof price.toFixed(4))
+
+// String into Int
+console.log(parseInt("123"))
+console.log(typeof parseFloat("123"))
+console.log(Number("123px")) // NaN
+console.log(typeof Number("123"))
+
