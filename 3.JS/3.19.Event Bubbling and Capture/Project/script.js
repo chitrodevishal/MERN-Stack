c968bbbf43f058374a5ffc8666d9c44adeea35a5
Vishal Chitrode <chitrodevishal@gmail.com>
2026-07-09 16:21:02 +0530

    Add HTML, CSS, and JavaScript files for background color changer project with event delegation

---
+++ 3.JS/3.19.Event Bubbling and Capture/Project/script.js
@@ -0,0 +1,47 @@
+// const red = document.getElementById("red");
+// const blue = document.getElementById("blue");
+// const green = document.getElementById("green");
+// const purple = document.getElementById("purple");
+// const orange = document.getElementById("orange");
+// const reset = document.getElementById("reset");
+
+// red.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "red";
+// });
+// blue.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "blue";
+// });
+// purple.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "purple";
+// });
+// orange.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "orange";
+// });
+// green.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "green";
+// });
+// reset.addEventListener("click", () => {
+//   document.body.style.backgroundColor = "black";
+// });
+
+
+// Here we get access of all button on once
+const buttons = document.querySelectorAll("button");
+// iterate over all button using for of loop
+// for (let button of buttons) {
+//   button.addEventListener("click", () => {
+//     console.log(button)
+//     console.log(button.id)
+//     document.body.style.backgroundColor = button.id;
+//   });
+// }
+
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    document.body.style.backgroundColor = button.id;
+  });
+})
+
+// Event Bubbling and Capturing
+
---
+++ 3.JS/3.19.Event Bubbling and Capture/Project1/script.js
@@ -0,0 +1,66 @@
+const grandparent = document.getElementById("grandparent")
+const parent = document.getElementById("parent")
+const child = document.getElementById("child")
+
+// Bubbling and Capture
+// by default Bubbling  (from inside to outside )
+// addEventListener(event, call back function, true/false (capturing)) // by default (from outside to inside)capturing false
+// child.addEventListener("click", () => {
+//     console.log("Child")
+ 
+// }, false )
+// parent.addEventListener("click", () => {
+//     console.log("Parent")
+   
+// }, false)
+// grandparent.addEventListener("click", () => {
+//     console.log("Grandparent")
+   
+// }, false)
+
+// capturing (from outside to inside)
+// child.addEventListener("click", () => {
+//     console.log("Child")
+ 
+// }, true )
+// parent.addEventListener("click", () => {
+//     console.log("Parent")
+   
+// }, true)
+// grandparent.addEventListener("click", () => {
+//     console.log("Grandparent")
+   
+// }, true)
+
+//random
+// child.addEventListener("click", () => {
+//     console.log("Child")
+ 
+// }, true )
+// parent.addEventListener("click", () => {
+//     console.log("Parent")
+   
+// }, false)
+// grandparent.addEventListener("click", () => {
+//     console.log("Grandparent")
+   
+// }, true)
+// OR
+child.addEventListener("click", (event) => {
+    console.log("Child")
+    console.log(event.target)
+    console.log(event.currentTarget)
+ 
+}, false )
+parent.addEventListener("click", (event) => {
+    console.log("Parent")
+    console.log(event.target)
+    console.log(event.currentTarget)
+   
+}, true)
+grandparent.addEventListener("click", (event) => {
+    console.log("Grandparent")
+    console.log(event.target)
+    console.log(event.currentTarget)
+   
+}, false)
