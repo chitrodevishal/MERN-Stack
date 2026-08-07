fffc167d2597ee9f39e010b49cbd46cca8f529a2
Vishal Chitrode <chitrodevishal@gmail.com>
2026-08-07 09:26:17 +0530

    feat: Add TypeScript files and configuration for initial setup

---
+++ 1.Frontend Development/4.TS/1.Intro/first.js
@@ -0,0 +1,22 @@
+"use strict";
+// let a:number = 10
+// let b:number = 20  // it work (The code we write in JavaScript will also run in TypeScript.)
+Object.defineProperty(exports, "__esModule", { value: true });
+// a = "Rohit" // Here error start showing, If we have written any valid code of Js in Ts then no one can stop it from converting into Js
+// Number
+let a = 10;
+let b = 20;
+// String
+let c = "Rohit";
+// Boolean
+let d = true;
+// Bigint
+let e = 123842758916345816405614835697348465820304700n;
+// null
+let f = null;
+// undefined
+let g;
+// g = "Vishal"
+let name = "Chitrode";
+let surname = "Chitrode";
+//# sourceMappingURL=first.js.map
---
+++ 1.Frontend Development/4.TS/1.Intro/first.ts
@@ -0,0 +1,26 @@
+// let a:number = 10
+// let b:number = 20  // it work (The code we write in JavaScript will also run in TypeScript.)
+
+// a = "Rohit" // Here error start showing, If we have written any valid code of Js in Ts then no one can stop it from converting into Js
+
+// Number
+let a: number = 10;
+let b: number = 20;
+
+// String
+let c: string = "Rohit";
+
+// Boolean
+let d: boolean = true;
+
+// Bigint
+let e: bigint = 123842758916345816405614835697348465820304700n;
+
+// null
+let f: null = null;
+
+// undefined
+let g: undefined;
+// g = "Vishal"
+let name: string = "Chitrode";
+let surname: string = "Chitrode";
